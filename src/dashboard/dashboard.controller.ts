a986af92401d567638d1737731246b1f3fa9f531
unknown <sopee_namyen@hotmail.com>
2020-06-08 12:01:02 +0700

    Get data

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -4,33 +4,18 @@ import { DashboardService } from './dashboard.service';
 @Controller('dashboard')
 export class DashboardController {
     constructor(private dashboardService: DashboardService) { }
-    //แก้ไข
     @Get('/getData') //showData
     async getData() {
         return await this.dashboardService.getData()
     }
 
-    // @Get('/getData/:id') //showData Id
-    // async getCar(@Param("id", ParseIntPipe) id: number, ) {
-    //     return await this.dashboardService.getCar(id);
-    // }
-
-    @Get('/getDashboard') //show Dashboard
-    async getDashboard() {
-        return await this.dashboardService.getDashboard();
+    @Get('/getData/:id') //showData Id
+    async getCar(@Param("id", ParseIntPipe) id: number, ) {
+        return await this.dashboardService.getCar(id);
     }
 
-    // @Get('/getRealtime') //show realtime
-    // async getRealtime() {
-    //     return await this.dashboardService.getRealtime()
-    // }
-
-    // @Get('/getGraph') //show realtime
-    // async getGraph() {
-    //     return await this.dashboardService.getGraph()
-    // }
-    @Get('/data') //showData
-    async data() {
-        return await this.dashboardService.data()
+    @Get('/dashboard') //show Dashboard
+    async getDashboard() {
+        return await this.dashboardService.getDashboard();
     }
 }
--- src/dashboard/dto/data-dashboard.dto.ts
+++ src/dashboard/dto/data-dashboard.dto.ts
@@ -1,31 +1,5 @@
 import { Moment } from "moment";
-
-export class Dashboard {
-    newDate: Moment;
-    totalCars: number;
-    carParking: number;
-    deliveryParking: number;
-    carVIP: number;
-    realtime: {
-        newDate: Moment;
-        id: number;
-        imgCar: string;
-        numberOfcars: string;
-        time: Moment;
-    }
-    graph: {
-        date: string;
-        totalCars: number;
-    }
-
-}
-export class CarDashboard {
-    newDate: Moment
-    totalCars: number;
-    carParking: number;
-    deliveryParking: number;
-    carVIP: number;
-}
+import * as moment from "moment";
 
 export class Realtime {
     newDate: Moment;
@@ -33,9 +7,45 @@ export class Realtime {
     imgCar: string;
     numberOfcars: string;
     time: Moment;
+
+    constructor() {
+        this.newDate = moment();
+        this.id = 0;
+        this.imgCar = '';
+        this.numberOfcars = '';
+        this.time = moment();
+    }
 }
 
 export class Graph {
     date: string;
     totalCars: number;
+
+    constructor() {
+        this.date = '';
+        this.totalCars = 0;
+    }
 }
+
+export class Dashboard {
+    newDate: Moment;
+    totalCars: number;
+    carParking: number;
+    deliveryParking: number;
+    carVIP: number;
+    realtime: Realtime[];
+    graph: Graph[];
+
+    constructor() {
+        this.newDate = moment();
+        this.totalCars = 0;
+        this.carParking = 0;
+        this.deliveryParking = 0;
+        this.carVIP = 0;
+
+        this.realtime = [new Realtime()];
+        this.graph = [new Graph()];
+    }
+}
+
+
