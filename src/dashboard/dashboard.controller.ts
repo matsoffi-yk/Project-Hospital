06604803c06bc67b70ec0321855b801cab2c1e4e
unknown <sopee_namyen@hotmail.com>
2020-06-08 12:02:12 +0700

    Get Dashboard

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -4,15 +4,6 @@ import { DashboardService } from './dashboard.service';
 @Controller('dashboard')
 export class DashboardController {
     constructor(private dashboardService: DashboardService) { }
-    @Get('/getData') //showData
-    async getData() {
-        return await this.dashboardService.getData()
-    }
-
-    @Get('/getData/:id') //showData Id
-    async getCar(@Param("id", ParseIntPipe) id: number, ) {
-        return await this.dashboardService.getCar(id);
-    }
 
     @Get('/dashboard') //show Dashboard
     async getDashboard() {
